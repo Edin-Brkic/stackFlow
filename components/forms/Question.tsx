6b8ae9853d6ccd1a1472df5f3feb50f7c794ec9e
EdinBrkic <edinbrkic26@gmail.com>
2024-06-20 00:24:49 +0200

    ddr

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -47,7 +47,7 @@ export default function Question({ mongoUserId }: Props) {
       title: "",
       explanation: "",
       tags: [],
-      author: JSON.parse(mongoUserId),
+      author: mongoUserId,
     },
   });
 
