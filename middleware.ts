a2628fff62ae04983d7263b88782494d5bfc25df
EdinBrkic <edinbrkic26@gmail.com>
2024-06-20 14:17:09 +0200

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+const ignoreRoutes = createRouteMatcher(["/api/webhook"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  // Proveravamo da li ruta treba da bude ignorisana
+  if (ignoreRoutes(req)) {
+    return; // Ako je ruta jedna od ignorisanih, preskačemo zaštitu
+  }
+
+  // Ako ruta treba da bude zaštićena, primenjujemo zaštitu
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
